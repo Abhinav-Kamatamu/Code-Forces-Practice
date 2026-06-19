b3ea992b08ff87263759db4ce608cd557ce5850e
Stranger <abhinav.kamatamu@gmail.com>
2026-06-19 03:43:36 -0400

    Median Partition solved

--- 1036C.cpp
+++ 1036C.cpp
@@ -1,4 +1,5 @@
 // Classy Numbers
+
 #include <iostream>
 #include <vector>
 #include <cmath>
@@ -6,79 +7,17 @@
 using namespace std;
 using ll = unsigned long long;
 
-int pick_no_1(int i, ll L, ll R) {
-    int count = 0;
-    for (int d1 = 1; d1 <= 9; d1++){
-        long long num = 0;
 
-        num += d1 * pow(10, i);
-        if (num >= L && num <= R)
-            count++;
-    }
-    return count;
-}
-
-int loopings(ll L, ll R, int ln, int rn) {
-    int count = 0;
-
-    for (int i = rn - 1; i >= ln - 1; i--) {
-        for (int d1 = 1; d1 <= 9; d1++) {
-            ll n1 = d1 * pow(10, i);
-            if (n1 >= L && n1 <= R)
-                count++;
-            for (int j = i - 1; j >= 0; j--) {
-                for (int d2 = 1; d2 <= 9; d2++) {
-                    ll n2 = d2 * pow(10, j);
-                    if (n1 + n2 >= L && n1 + n2 <= R)
-                        count++;
-                    for (int k = j - 1; k >= 0; k--) {
-                        for (int d3 = 1; d3 <= 9; d3++) {
-                            ll n3 = d3 * pow(10, k);
-                            if (n1 + n2 + n3 >= L && n1 + n2 + n3 <= R)
-                                count++;
-                        }
-                    } 
-                }
-            }
-        }
-    }
-
-    return count;
-}
+int 
 
 int main()
 {
     int t;
     cin >> t;
-    // ll outs[t];
-    for (int i = 0; i < t; i++) {
-        ll L, R;
 
-        cin >> L >> R;
-
-        // if(L == R){
-        //        // Case if L=R is classy
-        //        // Case if L=R is not classy
-        //       return 0;
-        // }
-
-        int l_n = 0;
-        int r_n = 0;
-        ll c = L;
-        for (l_n = 0; c > 0; l_n++)
-        {
-            c = c / 10;
-        }
-        c = R;
-        for (r_n = 0; c > 0; r_n++)
-        {
-            c = c / 10;
-        }
- 
-        cout << loopings(L, R, l_n, r_n) << endl;
+    while (t--) {
+        ll lower;
+        ll upper;
+        cin >> lower >> upper;
     }
-    // for (ll out : outs)
-    // {
-    //     cout << out << endl;
-    // }
-}
+} 
--- 2222C.cpp
+++ 2222C.cpp
@@ -6,34 +6,40 @@
 
 using namespace std;
 
+int generate_diff(const vector<int>& a, const int median, vector<int>& diff, vector<int>& median_count) {
+    /* Generates the diff array and returns the count of median elements */
 
-int foo(vector<int>& a, int median, int start, int depth, int tmc){
-    if(depth > tmc) return 0;
-    
-    int bc = 0;
-    int sc = 0;
-    int mc = 0;
+    int diff_counter = 0; // diff counter
+    int mc = 0; // median count
 
-    int biggest_partitions = 0;
-    for(int i = start; i < a.size(); i+=2){
-        if (a[i] > median) bc++;
-        else if (a[i] < median) sc++;
-        else mc++;
-
-        if (i - start > 0 && a[i - 1] > median) bc++;
-        else if (i - start > 0 && a[i - 1] < median) sc++;
-        else if (i - start > 0) mc++;
-
-        if (mc > 0 &&abs(bc - sc) <= mc) {
-            int current_partitions = foo(a, median, i + 1, depth + 1, tmc);
-
-            biggest_partitions = max(biggest_partitions, current_partitions + 1);
+    for(int i = 0; i < a.size(); i++){
+        if (a[i] == median){
+            mc++;
+            diff[i] = diff_counter;
+        } else if (a[i] < median){
+            diff[i] = --diff_counter;
+        } else {
+            diff[i] = ++diff_counter;
         }
+        median_count[i] = mc;
     }
-    return biggest_partitions;
+    return mc;
 }
 
+bool is_valid_part(const vector<int>& diff, const vector<int>& median_count, int start, int end) {
+    /* Checks if the partition from start to end is valid */
 
+    if ((end - start + 1) % 2 == 0) return false; // partition has to be odd
+
+    int medians_in_part = median_count[end] - (start > 0 ? median_count[start - 1] : 0);
+    int diff_in_part = diff[end] - (start > 0 ? diff[start - 1] : 0);
+
+    if (medians_in_part == 0) return false; // partition has to contain at least one median
+
+    if (abs(diff_in_part) < medians_in_part) return true; // valid partition if the absolute diff is less than or equal to the number of medians
+    return false;
+
+}
 
 int main() {
     int t;
@@ -42,26 +48,40 @@ int main() {
     while (t--){
         int n;
         cin >> n;
+        int median;
+        int mc = 0;
 
         vector<int> a(n);
-        vector<int> t(n);
+
+        // diff[i] = (greater than median - lesser than median) for all entries in range [1..i]
+        vector<int> diff(n);
+        vector<int> median_count(n, 0); // median_count[i] = number of median elements in range [1..i]
+        vector<int> temp(n); // temp duplicate of a for sorting
+        vector<int> max_partitions(n, 0); // max_partitions[i] = maximum number of median partitions in range [1..i]
 
         for(int i = 0; i < n; i++){
             cin >> a[i];
-            t[i] = a[i];
+            temp[i] = a[i];
         }
 
-        sort(t.begin(), t.end());
-        int median = t[n/2];
-        int mc = 0;
-        for(int i = 0; i < n; i++){
-            if (a[i] == median){
-                mc++;
-            }
+        sort(temp.begin(), temp.end());
+        median = temp[n/2];
 
+        mc = generate_diff(a, median, diff, median_count);
+
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j <= i; j++){
+                if (is_valid_part(diff, median_count, j, i)) {
+                    if (j == 0)
+                        max_partitions[i] = max(max_partitions[i], 1);
+                    else
+                        if (max_partitions[j-1] > 0) // Previous partitions have to be valid
+                            max_partitions[i] = max(max_partitions[i], max_partitions[j-1] + 1);
+                }
+            }
         }
         
 
-        cout << foo(a, median, 0, 1, mc) << endl;
+        cout << max_partitions[n-1] << endl;
     }
 }
